475afdb9a72d5d4fbfb4fe3b92f4f4905f3b129c
Ignacio <dev.ignacio.h@gmail.com>
2021-03-14 17:07:49 -0300

    validaciones 1
    	validar campos vacios y asignar id

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import Formulario from './components/Formulario'
+import Formulario from './components/Formulario';
 
 function App() {
   return (
     <>
-    <h1>Administrador de Pacientes</h1>
-    <div className="container">
-      <div className="row">
-        <div className="one-half column">
-          <Formulario />
-        </div>
-        <div className="one-half column">
-          2
+      <h1>Administrador de Pacientes</h1>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario />
+          </div>
+          <div className="one-half column">2</div>
         </div>
       </div>
-    </div>
     </>
   );
 }
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,14 +1,68 @@
+import { useState } from 'react';
+import { v4 as uuid } from 'uuid';
+
 const Formulario = () => {
+  // Crear State Citas
+  const [cita, actualizarCita] = useState({
+    mascota: '',
+    propietario: '',
+    fecha: '',
+    hora: '',
+    sintomas: '',
+  });
+
+  const [error, actualizarError] = useState(false);
+
+  // Función que se ejecuta cada vez que el usuario escribe en un input
+  const actualizarState = e => {
+    actualizarCita({
+      ...cita,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // Extraer los valores
+  const { mascota, propietario, fecha, hora, sintomas } = cita;
+
+  // Cuando el usuario presiona agregar cita
+  const submitCita = e => {
+    e.preventDefault();
+
+    // validar
+    const fields = [mascota, propietario, fecha, hora, sintomas];
+    const isEmpty = field => field.trim() === '';
+
+    if (fields.some(isEmpty)) {
+      actualizarError(true);
+      return;
+    }
+    // eliminar el mensaje previo
+    actualizarError(false);
+
+    // asignar ID
+    cita.id = uuid();
+    console.log(cita);
+
+    // crear la cita
+
+    // reiniciar el form
+  };
+
   return (
     <>
+      {error && (
+        <p className="alerta-error">Todos los campos son obligatorios</p>
+      )}
       <h2>Crear cita</h2>
-      <form>
+      <form onSubmit={submitCita}>
         <label htmlFor="">Nombre Mascota</label>
         <input
           type="text"
           name="mascota"
           className="u-full-width"
           placeholder="Nombre Mascota"
+          onChange={actualizarState}
+          value={mascota}
         />
         <label htmlFor="">Nombre Dueño</label>
         <input
@@ -16,14 +70,33 @@ const Formulario = () => {
           name="propietario"
           className="u-full-width"
           placeholder="Nombre dueño/a de la Mascota"
+          onChange={actualizarState}
+          value={propietario}
         />
         <label htmlFor="">Fecha alta</label>
-        <input type="date" name="fecha" className="u-full-width" />
+        <input
+          type="date"
+          name="fecha"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={fecha}
+        />
         <label htmlFor="">Hora</label>
-        <input type="time" name="hora" className="u-full-width" />
+        <input
+          type="time"
+          name="hora"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={hora}
+        />
         <label htmlFor="">Síntomas</label>
-        <textarea name="sintomas" className="u-full-width"></textarea>
-        <button type="submit" className="u-full-width button-primary">
+        <textarea
+          name="sintomas"
+          className="u-full-width"
+          onChange={actualizarState}
+          value={sintomas}
+        ></textarea>
+        <button type="submit" className="u-full-width button-primary  ">
           Agregar Cita
         </button>
       </form>
