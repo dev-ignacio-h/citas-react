2df57cbff93751bee21311ba54b154bbfc2a740f
Ignacio <dev.ignacio.h@gmail.com>
2021-03-14 18:12:15 -0300

    eliminar cita|

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Formulario from './components/Formulario';
+import Cita from './components/Cita';
 
 function App() {
   // Arreglo de citas
@@ -7,22 +8,29 @@ function App() {
 
   // Función que tome las citas actuales y agregue la nueva
   const crearCita = cita => {
-    guardarCitas([...citas, cita])
-  }
-  console.log(citas);
+    guardarCitas([...citas, cita]);
+  };
+
+  // Función que elimina una cita por su id
+  const eliminarCita = id => {
+    const nuevasCitas = citas.filter(cita => cita.id !== id);
+    guardarCitas(nuevasCitas)
+  };
+
   return (
-
-
     <>
       <h1>Administrador de Pacientes</h1>
       <div className="container">
         <div className="row">
           <div className="one-half column">
-            <Formulario
-              crearCita={crearCita}
-            />
+            <Formulario crearCita={crearCita} />
           </div>
-          <div className="one-half column">2</div>
+          <div className="one-half column">
+            <h2>Administra tus citas</h2>
+            {citas.map(cita => (
+              <Cita key={cita.id} cita={cita} eliminarCita={eliminarCita} />
+            ))}
+          </div>
         </div>
       </div>
     </>
